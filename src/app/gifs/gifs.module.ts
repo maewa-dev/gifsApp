e2f5d2ea1efbca85269f4a69a6e96e115fa1291d
Maewa <maewa.dev@gmail.com>
2023-02-01 20:43:01 +0100

    results component

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GifsPageComponent } from './gifs-page/gifs-page.component';
 import { SearchComponent } from './search/search.component';
+import { ResultsComponent } from './results/results.component';
 
 
 
@@ -9,13 +10,13 @@ import { SearchComponent } from './search/search.component';
   declarations: [
     GifsPageComponent,
     SearchComponent,
+    ResultsComponent,
   ],
   imports: [
     CommonModule
   ],
   exports: [
     GifsPageComponent,
-
   ]
 })
 export class GifsModule { }
