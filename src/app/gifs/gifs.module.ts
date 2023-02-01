f3c10c0fb7737bca0ae75369dbdce23854c291d7
Maewa <maewa.dev@gmail.com>
2023-02-01 20:34:52 +0100

    search component

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GifsPageComponent } from './gifs-page/gifs-page.component';
+import { SearchComponent } from './search/search.component';
 
 
 
 @NgModule({
   declarations: [
     GifsPageComponent,
+    SearchComponent,
   ],
   imports: [
     CommonModule
