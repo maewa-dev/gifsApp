c0578a8d6be0816644a702f587cb44cca44fd13c
Maewa <maewa.dev@gmail.com>
2023-02-05 18:23:51 +0100

    save and load from localStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -7,7 +7,15 @@ import { Gif, SearchGifResponse } from '../interface/gifs.interface';
 })
 export class GifsService {
 
-  constructor (private http: HttpClient) { }
+  constructor (private http: HttpClient) { 
+
+    //todo_mrt otra forma de hacerlo es ponerlo todo en una sola línea
+    // this._historial = JSON.parse(localStorage.getItem('hostorial)!) || []
+    if( localStorage.getItem('historial') ) {
+      this._historial = JSON.parse(localStorage.getItem('historial')! );
+    }
+    localStorage.getItem('historial');
+  }
 
   private apiKey: string = 'UXOcSsIo2kQ2hNUWwrfHpD01fO7jmdyV';
   private _historial: string[] = [];
@@ -15,20 +23,22 @@ export class GifsService {
   public results: Gif[] = [];
 
   get historial() {
-    return [...this._historial]
+    return [...this._historial];
   }
 
   searchGifs(query: string = '') {
     query = query.trim().toLowerCase();
 
     if ( !this._historial.includes(query) ) {
-      this._historial.unshift(query)
+      this._historial.unshift(query);
       this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
 
     this.http.get<SearchGifResponse>(`https://api.giphy.com/v1/gifs/search?api_key=UXOcSsIo2kQ2hNUWwrfHpD01fO7jmdyV&limit=10&q=${ query }`)
     .subscribe( (response) => {
-      this.results = response.data
+      this.results = response.data;
     }) 
 
   }
